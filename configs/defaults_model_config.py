5b32d74a1355a3fbffe0c75bbcdbbac2c2133370
SiddhanthHegde <siddhanthhegde227@gmail.com>
2022-03-23 17:09:12 +0530

    configs added for model arch, train and val

--- configs/defaults_model_config.py
+++ configs/defaults_model_config.py
@@ -27,3 +27,36 @@ _C.AUG.color_jitter_proba  = 0.7
 _C.AUG.grid_distort_proba  = 0.7 
 _C.AUG.guass_noise_proba   = 0.7 
 
+# -----------------------------------------------------------------------------
+# Model design
+# -----------------------------------------------------------------------------
+
+_C.MODEL = CN()
+_C.MODEL.model_arch = 'unet'
+_C.MODEL.encoder_name = 'resnet18'
+_C.MODEL.in_channels = 3
+_C.MODEL.classes = 3
+_C.MODEL.encoder_weights = 'imagenet'
+
+# -----------------------------------------------------------------------------
+# Train parameters
+# -----------------------------------------------------------------------------
+
+_C.TRAIN = CN()
+_C.TRAIN.batch_size = 1
+_C.TRAIN.shuffle = True
+_C.TRAIN.num_workers = 0
+_C.TRAIN.class_weights = []
+_C.TRAIN.lr = 2e-2
+_C.TRAIN.scheduler_patience = 2
+_C.TRAIN.lr_reduce_factor = 0.01
+_C.TRAIN.init_ckpt = ''
+
+# -----------------------------------------------------------------------------
+# Val parameters
+# -----------------------------------------------------------------------------
+
+_C.VAL = CN()
+_C.VAL.batch_size = 1
+_C.VAL.shuffle = False
+_C.num_workers = 0
