44a54180d148ab525160e855269371310c666a42
SiddhanthHegde <siddhanthhegde227@gmail.com>
2022-03-25 22:26:41 +0530

    Removed learning rate monitor and bug fixes

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
+from logging import config
 import os
 import wandb
 import argparse
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor  
 from pytorch_lightning import Trainer, seed_everything
 from model import SegmentationModel
 from utils import log_iou_dice
@@ -20,31 +21,25 @@ def main(cfg):
         'scheduler_patience': cfg.TRAIN.scheduler_patience,
         'lr_reduce_factor': cfg.TRAIN.lr_reduce_factor
     }
-    wandb_logger.log_hyperparams(hyperparams)
-    wandb_logger.watch(model)
     wandb_run_name = f"{cfg.MODEL.model_arch}_{cfg.MODEL.encoder_name}_{str(cfg.MODEL.encoder_weights)}"
     wandb_logger = WandbLogger(name=wandb_run_name,
                                 project=cfg.TRAIN.wandb_project_name)
+    wandb_logger.log_hyperparams(hyperparams)
     wandb_logger.watch(model)
-    lr_monitor = LearningRateMonitor(logging_interval='step')
+    # lr_monitor = LearningRateMonitor(logging_interval='step')
 
     ckpt_path = cfg.TRAIN.init_ckpt
 
-    if len(ckpt_path) != 0:
-        trainer = Trainer(logger=[wandb_logger, lr_monitor], 
-                        max_epochs=cfg.MODEL.n_epochs, 
-                        resume_from_checkpoint=ckpt_path,)
-    
-    else:
-        trainer = Trainer(logger=[wandb_logger, lr_monitor], 
-                        max_epochs=cfg.MODEL.n_epochs,)
+    trainer = Trainer(logger=[wandb_logger], 
+                    max_epochs=cfg.TRAIN.n_epochs,
+                    gpus=1)
 
-    trainer.fit(model)
-    ious, dice_sc = log_iou_dice(model, args)
+    trainer.fit(model, ckpt_path=ckpt_path)
+    ious, dice_sc = log_iou_dice(model, cfg)
     print('Iou: ', ious)
     print('Dice scores: ', dice_sc)
 
-    ckpt_path = os.path.join('./ckpt', f"{wandb_run_name}.ckpt")
+    ckpt_path = os.path.join('./ckpt', f"{wandb_run_name}_{cfg.TRAIN.n_epochs}.ckpt")
     ckpt_base_path = os.path.dirname(ckpt_path)
     os.makedirs(ckpt_base_path, exist_ok=True)
 
